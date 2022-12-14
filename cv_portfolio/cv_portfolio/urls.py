64834eb13984b44d8416cff172355beaee32fbbe
Cody Smith <courtlandrsmith@gmail.com>
2022-12-14 14:44:03 -0500

    link static files so styles.css works with my homepage

--- cv_portfolio/cv_portfolio/urls.py
+++ cv_portfolio/cv_portfolio/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
