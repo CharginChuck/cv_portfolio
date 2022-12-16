6b02d40e62af0995cb8a9d9974e796c9ce820e6b
Cody Smith <codeman1926@gmail.com>
2022-12-16 16:25:04 -0500

    added more skills from database to html file

--- cv_portfolio/pages/views.py
+++ cv_portfolio/pages/views.py
@@ -6,7 +6,14 @@ from .models import Skill, Subskill
 def home_view(request, *args, **kwargs):
     all_skills = Skill.objects.all()
     python_skill = all_skills.filter(title='Python')
+    webdev_skill = all_skills.filter(title='Web Development')
     all_subskills = Subskill.objects.all()
     python_subskills = all_subskills.filter(skill=1)
+    webdev_subskills = all_subskills.filter(skill=2)
+    sql_skill = all_skills.filter(title='SQL')
+    sql_subskills = all_subskills.filter(skill=3)
+    devops_skill = all_skills.filter(title='DevOps')
+    devops_subskills = all_subskills.filter(skill=5)
     
-    return render(request, "homepage.html", {'all':all_skills, 'python_subskills':python_subskills, 'python_skill':python_skill})
+    return render(request, "homepage.html", {'all':all_skills, 'python_subskills':python_subskills, 'python_skill':python_skill, 'webdev_subskills':webdev_subskills, 
+    'webdev_skill':webdev_skill, 'sql_subskills':sql_subskills, 'sql_skill':sql_skill, 'devops_subskills':devops_subskills, 'devops_skill':devops_skill})
