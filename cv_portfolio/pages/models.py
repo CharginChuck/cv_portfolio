050fcc3a00226f3789b4e011cc405513e0dd6328
Cody Smith <codeman1926@gmail.com>
2022-12-16 10:10:23 -0500

    altered the classes in models table and made a small update to the static html file

--- cv_portfolio/pages/models.py
+++ cv_portfolio/pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Contact(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=75)
+    company = models.CharField(max_length=50)
+    message = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname
